1ed0a0802ed03f3b9915d006c9e03139de82dc98
Mansi Jain <mj0196106@gmail.com>
2026-09-16 00:21:44 +0530

    Delete Build_Array_From_Permutation.c

--- Build_Array_From_Permutation.c
+++
@@ -1,9 +0,0 @@
-int* buildArray(int* nums, int numsSize, int* returnSize) {
-    int *ans=(int*)malloc(numsSize*sizeof (int));
-    for (int i=0;i<numsSize;i++)
-    {
-        ans[i]=nums[nums[i]];
-    }
-    *returnSize=numsSize;
-    return ans;
-}
